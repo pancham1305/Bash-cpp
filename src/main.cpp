79607f9397a18f0555d11e977bce0b44631017a8
Pancham Mehta <pancham1305@gmail.com>
2024-12-11 20:05:15 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+vector<string> split(string input)
+{
+  vector<string> v;
+  string s;
+  int n = input.size();
+  for (int i = 0; i < n; i++)
+  {
+    if (input[i] == ' ')
+    {
+      v.push_back(s);
+      s.clear();
+    }
+    else
+    {
+      s.push_back(input[i]);
+    }
+  }
+  if (!s.empty())
+  {
+    v.push_back(s);
+  }
+  return v;
+}
+
 int main()
 {
   // Flush after every cout / std:cerr
@@ -8,19 +33,33 @@ int main()
 
   // Uncomment this block to pass the first stage
   set<string> st;
-  st.insert("exit 0");
+  st.insert("exit");
+  st.insert("echo");
   while (1)
   {
     cout << "$ ";
     string input;
     getline(cin, input);
-    if (!st.count(input))
+    vector<string> v = split(input);
+    if (!st.count(v[0]))
     {
       cout << input << ": " << "not found" << endl;
+    }
+    else if (v[0] == "echo")
+    {
+      for (int i = 1; i < v.size(); i++)
+      {
+        cout << v[i] << ((i == v.size() - 1) ? "" : " ");
+      }
+      cout << endl;
     }
     else
     {
       break;
     }
+    // else
+    // {
+    //   break;
+    // }
   }
 }
