8b3d266e5745ee37f394d62f11fad6a1901f9eac
Pancham Mehta <pancham1305@gmail.com>
2024-12-13 16:25:50 +0530

    codecrafters submit [skip ci]

--- src/command.h
+++ src/command.h
@@ -5,12 +5,14 @@
 #include <vector>
 #include <filesystem>
 #include <iostream>
+#include <fstream>
 using namespace std;
 namespace fs = std::filesystem;
 class command
 {
 private:
-    std::map<std::string, std::function<int(std::vector<std::string>)>> mp;
+    std::map<std::string, std::function<int(std::vector<std::string>)>> shellBuiltins;
+    std::map<string, string> externalCommands;
     std::string env = getenv("PATH");
     std::vector<std::string> envVars;
 
@@ -37,46 +39,97 @@ public:
         }
         // for (auto i : envVars)
         // {
-        //     std::cout << i << " ";
+        //     cout << i << " ";
+        // }
+        // cout << endl;
+        for (auto &path : envVars)
+        {
+            int cnt = 0;
+            // cout << path << ": ";
+            if (path.substr(0, 4) == "/tmp")
+            {
+                // cout << "Executing...." << endl;
+                for (int i = 0; i < 10000000; i++)
+                    ;
+            }
+            for (auto file : (fs::recursive_directory_iterator(path)))
+            {
+                string tmp = file.path().filename().string();
+                cnt++;
+                if (externalCommands.find(tmp) != externalCommands.end())
+                {
+                    continue;
+                }
+                externalCommands[tmp] = file.path().string();
+                // if (path.substr(0, 4) == "/tmp")
+                // {
+                //     // cout << tmp << endl;
+                //     string cmd = tmp + " alice";
+                //     system(cmd.c_str());
+                //     cout << (externalCommands.find(tmp) != externalCommands.end()) << endl;
+                // }
+            }
+
+            // cout << cnt << endl;
+        }
+        // cout << externalCommands["cat"] << endl;
+        // cout << endl;
+        // for (auto i : externalCommands)
+        // {
+        //     cout << i.first << " ";
         // }
         // cout << endl;
     };
     void addCommand(std::string s, std::function<int(std::vector<std::string>)> f)
     {
-        mp[s] = f;
+        shellBuiltins[s] = f;
     }
 
     int executeCommand(std::vector<std::string> &v)
     {
         std::string c = v[0];
-        if (mp.find(c) != mp.end())
+        // cout << c << endl;
+        // if (externalCommands.find(c) != externalCommands.end())
+        // {
+        //     cout << ":')" << endl;
+        // }
+        // string s = "ls /tmp/pineapple";
+        // system(s.c_str());
+        if (c == "type")
         {
-            std::function<int(std::vector<std::string>)> f = mp[c];
-            if (c == "type")
+            if (shellBuiltins.find(v[1]) != shellBuiltins.end())
             {
-                if (mp.find(v[1]) != mp.end())
-                {
-                    v.erase(v.begin());
-                    return f(v);
-                }
-                // The game begins
-                for (auto &path : envVars)
-                {
-                    std::string s = path + "/" + v[1];
-                    // cout << s << endl;
-                    if (fs::is_regular_file(s))
-                    {
-                        v.clear();
-                        v.push_back(v[1]);
-                        v.push_back(s);
-                        return f(v);
-                    }
-                }
+                std::function<int(std::vector<std::string>)> f = shellBuiltins[c];
                 v.erase(v.begin());
-                return 0;
+                return f(v);
             }
+            else if (externalCommands.find(v[1]) != externalCommands.end())
+            {
+                cout << v[1] << " is " << externalCommands[v[1]] << endl;
+                return 1;
+            }
+            v.erase(v.begin());
+            return 0;
+        }
+        if (shellBuiltins.find(c) != shellBuiltins.end())
+        {
+            std::function<int(std::vector<std::string>)> f = shellBuiltins[c];
+
             int ret = f(v);
             return ret;
+        }
+        else if (externalCommands.find(c) != externalCommands.end())
+        {
+
+            string s;
+            for (int i = 0; i < v.size(); i++)
+            {
+                s.append(v[i]);
+                s.append(" ");
+            }
+            s.pop_back();
+            system(s.c_str());
+            return 1;
         }
         return 0;
     }
